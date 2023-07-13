b6f25d2f3f0402edbfa7e968ad615664c1134b7a
venom-10 <arohit0102@gmail.com>
2023-07-13 20:28:56 +0530

    completed routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, createComponent } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainTableComponent } from './pages/main-table/main-table.component';
 import { SearchUserComponent } from './pages/search-user/search-user.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { CreateComponent } from './pages/create/create.component';
+import { UpdateUserComponent } from './pages/update-user/update-user.component';
 
 const routes: Routes = [
   { path: '', component: MainTableComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'add', component: CreateComponent },
+  { path: 'search', component: SearchUserComponent },
+  { path: 'update', component: UpdateUserComponent },
 ];
 
 @NgModule({
