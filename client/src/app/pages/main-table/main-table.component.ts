3925b8f4408715a0f47c6b96f8237eaa6d3b125d
venom-10 <arohit0102@gmail.com>
2023-07-13 14:33:02 +0530

    rm node_modules from server

--- client/src/app/pages/main-table/main-table.component.ts
+++ client/src/app/pages/main-table/main-table.component.ts
@@ -4,36 +4,36 @@ import { data } from 'src/data';
 @Component({
   selector: 'app-main-table',
   templateUrl: './main-table.component.html',
-  styleUrls: ['./main-table.component.css']
+  styleUrls: ['./main-table.component.css'],
 })
 export class MainTableComponent {
-  usersData:data[] = [
+  usersData: data[] = [
     {
-      name:'Rohit',
-      email:'arohit0102@gmail.com',
-      gender:'Male',
-      state:'West Bengal',
-      address:'Kolkata',
-      dob:'01/02/2000',
-      image:''
+      name: 'Rohit',
+      email: 'arohit0102@gmail.com',
+      gender: 'Male',
+      state: 'West Bengal',
+      address: 'Kolkata',
+      dob: '01/02/2000',
+      image: '',
     },
     {
-      name:'Aftab',
-      email:'arohit0102@gmail.com',
-      gender:'Male',
-      state:'West Bengal',
-      address:'Kolkata',
-      dob:'01/02/2000',
-      image:''
+      name: 'Aftab',
+      email: 'arohit0102@gmail.com',
+      gender: 'Male',
+      state: 'West Bengal',
+      address: 'Kolkata',
+      dob: '01/02/2000',
+      image: '',
     },
     {
-      name:'Rion',
-      email:'arohit0102@gmail.com',
-      gender:'Male',
-      state:'West Bengal',
-      address:'Kolkata',
-      dob:'01/02/2000',
-      image:''
-    }
-  ]
+      name: 'Rion',
+      email: 'arohit0102@gmail.com',
+      gender: 'Male',
+      state: 'West Bengal',
+      address: 'Kolkata',
+      dob: '01/02/2000',
+      image: '',
+    },
+  ];
 }
