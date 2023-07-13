4e441b5c3fd98e3a356a0ddd6fe654370583bef7
venom-10 <arohit0102@gmail.com>
2023-07-13 19:51:06 +0530

    fixing the dropdown

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,6 +5,10 @@ import { initFlowbite } from 'flowbite';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
