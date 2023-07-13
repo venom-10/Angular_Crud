81112d177f41148358ab64435bb999baa9f5a0cb
venom-10 <arohit0102@gmail.com>
2023-07-13 15:46:57 +0530

    added database config

--- server/db/userAuthDb.js
+++ server/db/userAuthDb.js
@@ -1,15 +1,18 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+const path = require('path');
+dotenv.config({ path: path.join(__dirname, '../../.env') });
 
-const sequelize = new Sequelize('angular_crud', 'root', 'Root@123', {
-  dialect:'mysql'
+const sequelize = new Sequelize(process.env.DATABASE, process.env.NAME, process.env.PASSWORD, {
+  dialect: 'mysql'
 })
 
-const connect = async ()=>{
-  try{
+const connect = async () => {
+  try {
     await sequelize.authenticate();
     console.log('connection succesfull');
   }
-  catch(err){
+  catch (err) {
     console.log(err);
   }
 }
